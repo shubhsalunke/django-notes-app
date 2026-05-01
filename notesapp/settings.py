caeb93f56383c045cc7d2a77bb7888e545200551
Ubuntu <ubuntu@ip-172-31-32-209.ec2.internal>
2026-05-01 07:48:22 +0000

    Fix DB to SQLite and allowed hosts

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -89,13 +89,10 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 DATABASES = {
 
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
